67aa120284adc3a1d59c60da7b563cb0fcc7b178
pronayguha <pronay.guha@aot.edu.in>
2020-10-29 13:39:14 +0530

    Finish Project

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,18 @@
-window.onload(alert("This is the boiler-plate"));
+let changerButton = document.getElementById("changerButton");
+
+//function to genrate random color
+const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+//handler of mouse click
+const clickButton = () => {
+  document.body.style.backgroundColor = getRandomColor();
+};
+
+changerButton.addEventListener("click", clickButton);
